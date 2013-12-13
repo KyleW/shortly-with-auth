1400a931de84224faaa7dafab629f0896c54ae59
KyleW <Kyle.Warneck@gmail.com>
2013-12-13 15:39:37 -0800

    first attempts at rendering links

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-angular.module('myApp', [])
-  .controller('MyController', function($scope) {
-    $scope.name = "Kyle";
+angular
+  .module('myApp', [])
+  .controller('myController', function($scope, $http) {
+    // $scope.links="Here's where some links will go";
+    $http({
+      method: "GET",
+      url: "127.0.0.1:4567/links"
+    }).success(function(data){
+      console.log(data);
+      $scope.links = data;
+      // Resolve the cross origin issue
+    })
   });

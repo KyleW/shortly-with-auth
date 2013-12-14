564266b0e1fba0d8063595889dce3c56ccb60510
KyleW <Kyle.Warneck@gmail.com>
2013-12-14 11:34:27 -0800

    implementing angular routing - in progress

--- public/main.js
+++ public/main.js
@@ -1,28 +1,40 @@
-angular.module('myApp', [])
-  .controller('viewLinks', function($scope, $http) {
+angular.module('myApp', ['ngRoute','myControllers'])
+  .config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.
+        when('/#', {
+          templateUrl: 'templates/createLinks.html',
+          controller: 'createLinks'
+        }).
+        when('/add', {
+          templateUrl: 'partials/addLinks.html',
+          controller: 'viewLinks'
+        }).
+        otherwise({
+          redirectTo: '/'
+        });
+  }]);
+
+angular.module('myControllers',[]).controller('viewLinks', function($scope, $http) {
     $http({
       method: "GET",
       url: "http://localhost:4567/links"
     }).success(function(data){
       console.log(data);
       $scope.links = data;
-            // Resolve the cross origin issue
     });
-  }).controller('createLinks', function($scope, $http) {
+  })
+  .controller('createLinks', function($scope, $http) {
     $scope.submit = function() {
       var temp ={url: $scope.newURL};
-      // debugger;
-      // if (this.newUrl) {
-        // console.log(JSON.stringify(temp));
         $http({
           method: "POST",
           url: "http://localhost:4567/links",
           headers: {'Content-Type': 'application/json'},
           data: JSON.stringify(temp)
         }).then(function(){
-          console.log("sent in a new link.")
+          console.log("sent in a new link.");
           $scope.newURL="";
-        })
-      // }
+        });
     };
   });

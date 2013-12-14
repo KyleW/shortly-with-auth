cd7f83e0cc45d8ef4ac747c64038ba6ff56c496a
KyleW <Kyle.Warneck@gmail.com>
2013-12-13 17:30:09 -0800

    post in progress

--- public/main.js
+++ public/main.js
@@ -1,13 +1,20 @@
- angular
-    .module('myApp', [])
-    .controller('myController', function($scope, $http) {
-      $scope.working="Here's where some links will go";
-      $http({
-        method: "GET",
-        url: "http://localhost:4567/links"
-      }).success(function(data){
-        console.log(data);
-        $scope.links = data;
+angular
+  .module('myApp', [])
+  .controller('viewLinks', function($scope, $http) {
+    $http({
+      method: "GET",
+      url: "http://localhost:4567/links"
+    }).success(function(data){
+      console.log(data);
+      $scope.links = data;
             // Resolve the cross origin issue
-          })
+    })
+  })
+  .controller('createLinks', function($scope, $http) {
+    $scope.newLink = function(){
+      $http({
+      method: "POST",
+      url: "http://localhost:4567/links"
+      data:
+    })
   });

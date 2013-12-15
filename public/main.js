6ba44fc9d9a36399c8eef35be07e9ed3b47668b0
KyleW <Kyle.Warneck@gmail.com>
2013-12-14 17:38:32 -0800

    sorting in reverse order now and populated some data

--- public/main.js
+++ public/main.js
@@ -9,33 +9,33 @@ angular.module('myApp', ['ngRoute','myControllers'])
         when('/add', {
           templateUrl: 'templates/createLinks.html',
           controller: 'createLinks'
-        }).
-        when('/login', {
-          templateUrl: 'templates/createLinks.html',
-          controller: 'loginController'
-        }).
-        when('/signup', {
-          templateUrl: 'templates/createLinks.html',
-          controller: 'loginController'
+        // }).
+        // when('/login', {
+        //   templateUrl: 'templates/createLinks.html',
+        //   controller: 'loginController'
+        // }).
+        // when('/signup', {
+        //   templateUrl: 'templates/createLinks.html',
+        //   controller: 'loginController'
         }).
         otherwise({
           redirectTo: '/'
         });
-  }])
-  .run(function($rootScope, $location, Session){
-    $rootScope.$on('$routeChangeStart', function(evt, nextURL, currentUrl){
-      if(nextUrl.$$route.controller !== 'loginController' && !Session.isLoggedIn()){
-        console.log("No Auth");
-        $location.path('/login');
+  }]);
+  // .run(function($rootScope, $location, Session){
+  //   $rootScope.$on('$routeChangeStart', function(evt, nextURL, currentUrl){
+  //     if(nextUrl.$$route.controller !== 'loginController' && !Session.isLoggedIn()){
+  //       console.log("No Auth");
+  //       $location.path('/login');
 
-      }
-    });
-  })
-  .factory('Session', function($q, $http){
-    var service = {
-      currentUser: null,
-      isLoggedIn: function(){
+  //     }
+  //   });
+  // })
+  // .factory('Session', function($q, $http){
+  //   var service = {
+  //     currentUser: null,
+  //     isLoggedIn: function(){
 
-      }
-    };
-  });
+  //     }
+  //   };
+  // });
